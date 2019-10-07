bfbf4d2ec06d9ff96044e232115da07222992701
Alan Wei <alan.wei@live.com>
2019-10-07 19:23:02 +0800

    build: remove ant-design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './modules/app-routing.module';
-import { NgZeroModule } from './modules/ng-zero.module';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
 import { FooterComponent } from './footer/footer.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoCreateComponent } from './todo-create/todo-create.component';
-
-registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -28,7 +23,6 @@ registerLocaleData(en);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgZeroModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule
